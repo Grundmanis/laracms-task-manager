601c3d50a657ab40befaa899e3540d8ba6561d59
Armand Grundmanis <agrundmanis@inbox.lv>
2018-04-28 19:21:18 +0300

    upd composer.json

--- src/laracms_task_routes.php
+++ src/laracms_task_routes.php
@@ -6,7 +6,7 @@ Route::group([
     'prefix'     => 'laracms/task-manager'
 ], function () {
     Route::get('/', 'TaskController@index')->name('laracms.task');
-    Route::get('/{task}', 'TaskController@show')->name('laracms.task.show');
+    Route::get('/show/{task}', 'TaskController@show')->name('laracms.task.show');
     Route::get('/create', 'TaskController@create')->name('laracms.task.create');
     Route::post('/create', 'TaskController@store');
     Route::get('/edit/{task}', 'TaskController@edit')->name('laracms.task.edit');
